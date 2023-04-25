a20dc753814c583e8c090b4bc86ff8b7fa86470e
Andres Tangarife <andresfelta95@gmail.com>
2023-04-25 11:19:04 -0600

    comments in the constants

---
+++ src/constants/Constants.js
@@ -0,0 +1,8 @@
+// Constants to give a range of loctions for each section of the dart board
+// The locations are based on the ESP32 coordinates:
+// 0,0 is the center of the board
+// 19,0 is the right edge of the board
+// 0,19 is the top edge of the board
+// -19,0 is the left edge of the board
+// 0,-19 is the bottom edge of the board
+//
